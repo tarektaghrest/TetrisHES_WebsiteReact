aed9493fe5e8da01bd05c305a08da6f90d8431c1
tarek <tarek.taghrest@hes-so.ch>
2025-12-22 14:59:07 +0100

    Update asset paths in index.html to include Tetris-HES-SO prefix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 
-  base: "/Tetris/",
+  base: "/Tetris-HES-SO/",
   build: {
     outDir: "docs",
   },
