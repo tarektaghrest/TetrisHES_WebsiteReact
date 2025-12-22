3e1dc233bcfe8048099bc0fb05c6736aca018e2c
tarek <tarek.taghrest@hes-so.ch>
2025-12-22 15:09:55 +0100

    Refactor code structure for improved readability and maintainability

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import "./Footer.css";
+import logo from "../assets/logo.png";
 
 function Footer() {
-  return (
-    <footer className="site-footer">
-      <img src="/logo.png" alt="HES-SO logo" className="footer-logo" />
-    </footer>
-  );
+  return <img src={logo} alt="HES-SO logo" className="footer-logo" />;
 }
 
 export default Footer;
