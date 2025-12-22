e219eb32116007ddfd9cf0f5d8767f2a6923c04b
tarek <tarek.taghrest@hes-so.ch>
2025-12-22 15:16:05 +0100

    Refactor code structure for improved readability and maintainability

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 
-  base: "/Tetris-HES-SO/",
+  base: "/Tetris/",
   build: {
     outDir: "docs",
   },
