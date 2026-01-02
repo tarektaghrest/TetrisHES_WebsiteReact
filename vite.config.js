f9de436c4388f043eb5cdd0dd670714f1467ccc1
Théo Mayoraz <theo.mayoraz@outlook.fr>
2026-01-02 14:22:00 +0100

    Config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 
-  base: "/Tetris/",
+  base: "/TetrisHES_WebsiteReact/",
   build: {
     outDir: "docs",
   },
